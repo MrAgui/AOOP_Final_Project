9d1af6de0ef5b7ca8d6cfae43977028066da2716
MrAgui <karlaguilar3800@gmail.com>
2022-05-05 17:04:47 +0800

    Test

--- Student_Manangement_System.py
+++ Student_Manangement_System.py
@@ -1,1 +1,24 @@
-print("Hello World")
+# ==================imports===================
+import sqlite3
+from tkinter import *
+
+# ============================================
+
+root = Tk()
+root.geometry("1366x768")
+root.title("Intro")
+root.resizable(0, 0)
+
+global student_window
+global page1
+student_window = Toplevel()
+
+
+
+class Student:
+    print("Test Class")
+
+root.withdraw()
+page1 = Student(student_window)
+page1.time()
+root.mainloop()
