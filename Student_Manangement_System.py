2baca879583118cbe0783f7c6bc51ae99655e397
MrAgui <karlaguilar3800@gmail.com>
2022-05-05 17:34:39 +0800

    Fix file path error

--- Student_Manangement_System.py
+++ Student_Manangement_System.py
@@ -15,7 +15,7 @@ student_window = Toplevel()
 
 
 #Connection to database
-with sqlite3.connect("C:\\Users\\Gigabyte\\Desktop\\2nd SY. 2021-2022\\Advance-OOP\\Final_Project\\Database\\student.db") as db:
+with sqlite3.connect("C:\\Users\\Gigabyte\\Desktop\\2nd SY. 2021-2022\\Advance-OOP\\FinalProject\\Database\\student.db") as db:
     cur = db.cursor()
 
 
@@ -28,7 +28,7 @@ class Student:
 
         self.window_title = Label(student_window)
         self.window_title.place(relx=0, rely=0, width=1366, height=768)
-        self.img = PhotoImage(file="C:\\Users\\Gigabyte\\Desktop\\2nd SY. 2021-2022\\Advance-OOP\\Final_Project\\img\\StudentManageMentUI.png")
+        self.img = PhotoImage(file="C:\\Users\\Gigabyte\\Desktop\\2nd SY. 2021-2022\\Advance-OOP\\FinalProject\\img\\StudentManageMentUI.png")
         self.window_title.configure(image=self.img)
 
         self.final_project = Label(student_window)
@@ -50,6 +50,70 @@ class Student:
         self.search_entry.configure(font="-family {Poppins} -size 12")
         self.search_entry.configure(relief="flat")
 
+        self.search_std_btn = Button(student_window)
+        self.search_std_btn.place(relx=0.232, rely=0.288, width=75, height=22)
+        self.search_std_btn.configure(relief="flat")
+        self.search_std_btn.configure(overrelief="flat")
+        self.search_std_btn.configure(activebackground="#5BB2FE")
+        self.search_std_btn.configure(cursor="hand2")
+        self.search_std_btn.configure(foreground="#ffffff")
+        self.search_std_btn.configure(background="#5BB2FE")
+        self.search_std_btn.configure(font="-family {Poppins SemiBold} -size 10")
+        self.search_std_btn.configure(borderwidth="0")
+        self.search_std_btn.configure(text="""Search""")
+        self.search_std_btn.configure(command=self.search_student)
+
+        self.add_std_btn = Button(student_window)
+        self.add_std_btn.place(relx=0.052, rely=0.426, width=306, height=28)
+        self.add_std_btn.configure(relief="flat")
+        self.add_std_btn.configure(overrelief="flat")
+        self.add_std_btn.configure(activebackground="#5BB2FE")
+        self.add_std_btn.configure(cursor="hand2")
+        self.add_std_btn.configure(foreground="#ffffff")
+        self.add_std_btn.configure(background="#5BB2FE")
+        self.add_std_btn.configure(font="-family {Poppins SemiBold} -size 12")
+        self.add_std_btn.configure(borderwidth="0")
+        self.add_std_btn.configure(text="""Add Student""")
+        self.add_std_btn.configure(command=self.add_student)
+
+        self.update_std_btn = Button(student_window)
+        self.update_std_btn.place(relx=0.052, rely=0.5, width=306, height=28)
+        self.update_std_btn.configure(relief="flat")
+        self.update_std_btn.configure(overrelief="flat")
+        self.update_std_btn.configure(activebackground="#5BB2FE")
+        self.update_std_btn.configure(cursor="hand2")
+        self.update_std_btn.configure(foreground="#ffffff")
+        self.update_std_btn.configure(background="#5BB2FE")
+        self.update_std_btn.configure(font="-family {Poppins SemiBold} -size 12")
+        self.update_std_btn.configure(borderwidth="0")
+        self.update_std_btn.configure(text="""Update Student""")
+        self.update_std_btn.configure(command=self.update_student)
+
+        self.delete_std_btn = Button(student_window)
+        self.delete_std_btn.place(relx=0.052, rely=0.57, width=306, height=27)
+        self.delete_std_btn.configure(relief="flat")
+        self.delete_std_btn.configure(overrelief="flat")
+        self.delete_std_btn.configure(activebackground="#5BB2FE")
+        self.delete_std_btn.configure(cursor="hand2")
+        self.delete_std_btn.configure(foreground="#ffffff")
+        self.delete_std_btn.configure(background="#5BB2FE")
+        self.delete_std_btn.configure(font="-family {Poppins SemiBold} -size 12")
+        self.delete_std_btn.configure(borderwidth="0")
+        self.delete_std_btn.configure(text="""Delete Student""")
+        self.delete_std_btn.configure(command=self.delete_student)
+
+        self.Exit_btn = Button(student_window)
+        self.Exit_btn.place(relx=0.135, rely=0.883, width=76, height=21)
+        self.Exit_btn.configure(relief="flat")
+        self.Exit_btn.configure(overrelief="flat")
+        self.Exit_btn.configure(activebackground="#5BB2FE")
+        self.Exit_btn.configure(cursor="hand2")
+        self.Exit_btn.configure(foreground="#ffffff")
+        self.Exit_btn.configure(background="#5BB2FE")
+        self.Exit_btn.configure(font="-family {Poppins SemiBold} -size 12")
+        self.Exit_btn.configure(borderwidth="0")
+        self.Exit_btn.configure(text="""EXIT""")
+        self.Exit_btn.configure(command=self.Exit)
 
 
 root.withdraw()
